59a61459f6c664a1094afa1082029d84d63bde8a
Vadim Zmiievskyi <prainua@gmail.com>
2020-08-26 12:36:53 +0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
